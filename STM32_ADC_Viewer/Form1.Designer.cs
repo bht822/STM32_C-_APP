fe5dcb504f420981d0a437c1ad48f21f2682b373
Brad Trehan <bradt@vogcalgaryappdeveloper.com>
2021-07-27 23:58:23 -0600

    Add project files.

---
+++ STM32_ADC_Viewer/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace STM32_ADC_Viewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ComPortList = new System.Windows.Forms.ComboBox();
+            this.btInitialize = new System.Windows.Forms.Button();
+            this.baudRate = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "COM PORTS";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(29, 213);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(408, 212);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(542, 326);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "V";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // ComPortList
+            // 
+            this.ComPortList.FormattingEnabled = true;
+            this.ComPortList.Location = new System.Drawing.Point(128, 48);
+            this.ComPortList.Name = "ComPortList";
+            this.ComPortList.Size = new System.Drawing.Size(268, 25);
+            this.ComPortList.TabIndex = 1;
+            this.ComPortList.SelectedIndexChanged += new System.EventHandler(this.ComPortList_SelectedIndexChanged);
+            // 
+            // btInitialize
+            // 
+            this.btInitialize.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btInitialize.Location = new System.Drawing.Point(412, 97);
+            this.btInitialize.Name = "btInitialize";
+            this.btInitialize.Size = new System.Drawing.Size(134, 39);
+            this.btInitialize.TabIndex = 2;
+            this.btInitialize.Text = "Connect";
+            this.btInitialize.UseVisualStyleBackColor = true;
+            this.btInitialize.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // baudRate
+            // 
+            this.baudRate.FormattingEnabled = true;
+            this.baudRate.ItemHeight = 17;
+            this.baudRate.Location = new System.Drawing.Point(412, 56);
+            this.baudRate.Name = "baudRate";
+            this.baudRate.Size = new System.Drawing.Size(134, 21);
+            this.baudRate.TabIndex = 5;
+            this.baudRate.SelectedIndexChanged += new System.EventHandler(this.baudRate_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.baudRate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btInitialize);
+            this.Controls.Add(this.ComPortList);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox ComPortList;
+        private System.Windows.Forms.Button btInitialize;
+        private System.Windows.Forms.ListBox baudRate;
+    }
+}
+
---
+++ STM32_ADC_Viewer/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace STM32_ADC_Viewer
+{
+    public partial class Form1 : Form
+    {
+        // Placeholder for the selected port
+        public String SelectedPort;
+        // Placeholder to keep the read ports
+        string[] ArrayComPortNames = null;
+        int index = -1;
+        string ComPortName = null;
+        SerialPort STM32Port = new SerialPort();
+
+        public Form1()
+        {
+            InitializeComponent();
+            ArrayComPortNames = SerialPort.GetPortNames();
+            ComPortList.DataSource = ArrayComPortNames;
+            baudRate.Items.Add(300);
+            baudRate.Items.Add(600);
+            baudRate.Items.Add(1200);
+            baudRate.Items.Add(2400);
+            baudRate.Items.Add(9600);
+            baudRate.Items.Add(19200);
+            baudRate.Items.Add(115200);
+
+
+
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            STM32Port.BaudRate = 115200;
+            STM32Port.Parity = Parity.None;
+            STM32Port.StopBits = StopBits.One;
+            STM32Port.PortName = ComPortList.Text.ToString();
+            STM32Port.DataBits = 8;
+            STM32Port.Open();
+            while (STM32Port.IsOpen) {
+
+                richTextBox1.Text = STM32Port.ReadLine ();
+            
+            }
+
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComPortList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void baudRate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+
+}
